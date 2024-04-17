6ed9ff7d24d537e9dc528977894061b9a4523df5
Ben Timby <btimby@gmail.com>
2024-04-16 22:09:05 -0400

    Setup.py for python client.

---
+++ client/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="dink247",
+    version="1.0.0",
+    description="URL shortener",
+    author="Ben Timby",
+    author_email="btimby@247dink.com",
+    packages=["dink247"],
+    install_requires=[
+        'requests',
+    ],
+)
